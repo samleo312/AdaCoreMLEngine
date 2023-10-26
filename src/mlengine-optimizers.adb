95621397fb9358b1dbe26795b4f81276d1c697be
Owen Wienczkowski <wxyzowen@gmail.com>
2023-10-26 13:00:28 -0700

    update to sgd

--- src/mlengine-optimizers.adb
+++ src/mlengine-optimizers.adb
@@ -9,8 +9,8 @@ package body Mlengine.Optimizers is
 
 type SGD is tagged record
     lr, weight_decay, momentum : double;
-    -- parameters : arary of parameters for optimizer
-    -- velocity : array same size as "parameters" list but all values are 0
+    -- parameters : Float_Array (array of parameters for optimizer)
+    -- velocity : Float_Array (array same size as "parameters" list but all values are 0)
 end record;
 
    -- procedure with no parameters
@@ -18,7 +18,7 @@ end record;
     -- loops though instance variables "parameters" and "velocity"
     -- assigns new value to velocity
     -- assigns new value to parameters.data
-    procedure step()
+    procedure step (Float_Array: in out parameters, Float_Array: in out velocity)
     is
     begin
     -- cannot determine logic until parameters and velocity are concrete
@@ -27,6 +27,13 @@ end record;
         -- velocity[i] = momentum * velocity[i] + parameters.grad + weight_decay * parameters.data
         -- parameters.data = parameters.data - lr * velocity[i]
 
+    end
+
+    
+    procedure zeroGrad(Float_Array: in out parameters)
+    begin
+    -- must traverse array
+    end
 end Mlengine.Optimizers;
 
 
--- src/mlengine-optimizers.ads
+++ src/mlengine-optimizers.ads
@@ -2,10 +2,10 @@ package Mlengine.Optimizers is
     
     -- is constructor required? if so, only parameter required is "parameters"
 
-    -- procedure with no parameters, unique to each specific optimizer
-    procedure step ( )
+    -- procedure to adjust values of parameters and velocities for an adjustment towards the optimal solution
+    procedure step (Float_Array: in out parameters, Float_Array: in out velocity)
 
-    -- procedure to reset parameters, has no parameters
-    procedure zeroGrad( )
+    -- procedure to reset parameters to 0
+    procedure zeroGrad(Float_Array: in out parameters)
 
 end Mlengine.Optimizers;
