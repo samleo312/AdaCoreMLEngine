1337f18e526ef7c615c7284fdd017dc48537b202
ohenley <olivier.henley@gmail.com>
2023-11-07 11:47:21 -0500

    use tensor cpu

--- src/mlengine-utilities.adb
+++ src/mlengine-utilities.adb
@@ -2,10 +2,10 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 package body Mlengine.Utilities is
     procedure Test is
-        Floats : Float_Array := (1.0, 2.0, 3.0);
+        Floats : ST.Element_Array := (1.0, 2.0, 3.0);
     begin
         for F of Floats loop
-            Put_Line(Float'Image(F));
+            Put_Line(F'Image);
         end loop;
     end;
 end Mlengine.Utilities;
--- src/mlengine.ads
+++ src/mlengine.ads
@@ -1,6 +1,12 @@
-with Orka.Numerics.Tensors;
+with Orka.Numerics.Singles.Tensors;
+with Orka.Numerics.Singles.Tensors.CPU;
 
 package Mlengine is
-    package Numeric_Tensors is new Orka.Numerics.Tensors (Float);
-    subtype Float_Array is Numeric_Tensors.Element_Array;
+
+    package ST renames Orka.Numerics.Singles.Tensors;
+    package ST_CPU renames Orka.Numerics.Singles.Tensors.CPU;
+
+    Values : constant ST.Element_Array := (1.0, 2.0, 3.0, 4.0, 5.0);
+    Tensor : constant ST_CPU.CPU_Tensor := ST_CPU.To_Tensor (Values);
+
 end Mlengine;
