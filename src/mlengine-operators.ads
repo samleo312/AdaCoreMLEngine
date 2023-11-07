625c591c1699bbe93fb9be63414cec8d34eda8c3
garrett <gmg5511@psu.edu>
2023-11-06 19:24:51 -0800

    added pragrams to help compile

--- src/mlengine-operators.ads
+++ src/mlengine-operators.ads
@@ -1,7 +1,8 @@
 package Mlengine.Operators is
 
-    function Forward (Self: in Linear; Output: out Integer);
-    function Backward (Self: in Linear; GradInput: out Integer);
-    function GetParams(Self: in Linear; ParameterList: out Integer);
+    pragma Elaborate_Body;
+    --  function Forward (Self: in Linear; Output: out Integer);
+    --  function Backward (Self: in Linear; GradInput: out Integer);
+    --  function GetParams(Self: in Linear; ParameterList: out Integer);
 
 end Mlengine.Operators;
