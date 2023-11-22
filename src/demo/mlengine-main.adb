2351515663499bde4e9f3a380cd8b3b8f6665806
Owen Wienczkowski <wxyzowen@gmail.com>
2023-11-22 15:53:52 -0800

    first implementaion optimizers

--- src/demo/mlengine-main.adb
+++ src/demo/mlengine-main.adb
@@ -1,5 +1,6 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Mlengine.Utilities;
+with Mlengine.Optimizers;
 procedure Mlengine.Main is
 begin
     Put_Line ("Running Mlengine.Main");
--- src/mlengine-optimizers.adb
+++ src/mlengine-optimizers.adb
@@ -1,9 +1,3 @@
----------------------
--      Questions    -
----------------------
---unit tests?
---create Parameter_Array?
-
 with Orka; --for Float32 type
 use Orka; --for operator
 with Orka.Numerics.Singles.Tensors.CPU;
@@ -14,26 +8,29 @@ use Orka.Numerics.Singles.Tensors.CPU;
 package body Mlengine.Optimizers is
 
     -- procedure with no input
-    -- loops though instance variables "parameters" and "velocity"
+    -- loops though instance variables "params" and "velocity"
     -- assigns new value to velocity
-    -- assigns new value to parameters(1) (data)
-    procedure step (parameters: in out Tensor, velocity: in out Float_Array)
-    is
+    -- assigns new value to params(1) (t.data)
+    overriding procedure step (params: in out SGD) is
+    grad: ST_CPU.CPU_Tensor := params.t.Grad.all;
+    data: ST_CPU.CPU_Tensor := params.t.Data.all;
     begin
-        for i in velocity'Range loop
-            velocity(i) := momentum * velocity(i) + parameters(2)(i) + weight_decay * parameters(1)(i);
-            parameters(1)(i) := parameters(1)(i) - lr * velocity(i);
-        end loop
-    end
-
-
---procedure to reset all parameter gradient values to 0
-    procedure zero_grad(Float_Array: in out parametersGrad, Float_Array: in out parametersData)
-    begin
-        for i in parameters(2)'Range loop
-            parameters(2)(i) := 0;
+        for i in params.velocity'Range loop
+            declare
+                res: ET := data.get(i) - ET(params.lr) * params.velocity(i);
+            begin
+                params.velocity(i) := ET(params.momentum) * params.velocity(i) + grad.get(i) + ET(params.weight_decay) * data.get(i);
+                data.set(i, res);
+            end;
         end loop;
-    end zero_grad
+    end;
+
+
+--procedure to reset all parameter t.gradient values to 0
+    overriding procedure zero_grad(params : in out SGD) is
+    begin
+        params.t.Grad.all := ST_CPU.zeros(params.t.Grad.Elements);
+    end zero_grad;
 
 end Mlengine.Optimizers;
 
