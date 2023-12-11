e11238f807ec722fd2b6c2dc068e872eaf051e9a
samleo312 <samleo312@gmail.com>
2023-12-11 13:34:20 -0800

    fixed aunit

--- src/mlengine-utilities.adb
+++ src/mlengine-utilities.adb
@@ -1,26 +1,15 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Mlengine.Operators;
-with Linear_Tests; use Linear_Tests;
-with Relu_Tests; use Relu_Tests;
+with AUnit.Reporter.Text;
+with AUnit.Run;
+with Linear_Suite; 
 with Orka; use Orka;
 
 package body Mlengine.Utilities is
     procedure Test is
-        Test_Linear : Linear_Test;
-        Test_ReLU : relutest;
+        procedure Runner is new AUnit.Run.Test_Runner (Linear_Suite.Linear_Suite);
+        Reporter : AUnit.Reporter.Text.Text_Reporter;
     begin  
-        
-        Put_Line("Running Linear Tests");
-        Test_Forward(Test_Linear);
-        Test_Backward(Test_Linear);
-        Test_GetParams(Test_Linear);
-        Put_Line("Linear Tests Complete");
-
-        Put_Line("Running ReLU Tests");
-        Test_Forward_R(Test_ReLU);
-        Test_Backward_R(Test_ReLU);
-        Put_Line("ReLU Tests Complete");
-
-        
+        Runner (Reporter);
     end;
 end Mlengine.Utilities;
