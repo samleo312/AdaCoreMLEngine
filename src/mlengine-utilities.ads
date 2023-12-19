835f35dda9067b5f8f13cfacf5a03f1e8dbf6ea8
samleo312 <samleo312@gmail.com>
2023-12-18 18:45:28 -0800

    model skeleton added

--- src/mlengine-utilities.ads
+++ src/mlengine-utilities.ads
@@ -1,3 +1,22 @@
+with Mlengine.Operators; use Mlengine.Operators;
+with Ada.Containers;
+with Ada.Containers.Vectors;
+
 package Mlengine.Utilities is
-   procedure Test;
+
+   package Integer_Vectors is new
+     Ada.Containers.Vectors
+       (Index_Type   => Natural,
+        Element_Type => Func_Access_T);
+
+   use Integer_Vectors;
+
+   type Model is record
+      Graph : Vector;
+   end record;
+
+   procedure Add(M : Model);
+   function Fit(M: Model; Data : Integer; Target : Integer; Batch_Size : Integer; Num_Epochs : Integer; Optimizer : Integer; Loss_Fn : Integer);
+   function Predict(M : Model; Data : Integer);
+
 end Mlengine.Utilities;
