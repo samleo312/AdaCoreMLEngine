da00d0850be4bcd76fcd5167def642586399fec0
garrett <gmg5511@psu.edu>
2024-02-01 19:33:28 -0800

    loss forward done

--- src/mlengine-lossfunctions.adb
+++ src/mlengine-lossfunctions.adb
@@ -8,13 +8,38 @@ with Orka.Numerics.Singles.Tensors.CPU; use Orka.Numerics.Singles.Tensors.CPU;
 package body Mlengine.LossFunctions is
 
    --!!!!!!!!!!!!!!!!!!!!theres going to be conflicts we need to resolve between Orka Float_32, and Numerics Float!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-   function Forward (X : in out ST_CPU.CPU_Tensor; Target : in array (1 .. 20) of Orka.Float_32) return Orka.Float_32 is
+   function Forward (E: in out SoftLossMax_T; X : in out ST_CPU.CPU_Tensor; Target : in array (1 .. 20) of Orka.Float_32) return Orka.Float_32 is
       maximum : Orka.Float_32 := 0.0;
-      array_counter : Integer := 1;
+      Maximums : array (1 .. 20) of Orka.Float_32;
+      Losses : array (1 .. 20) of Orka.Float_32;
+      Unnormalized_Proba : ST_CPU.CPU_Tensor := ST.CPU.To_Tensor((0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0,
+                                                                  0.0, 0.0, 0.0
+                                                                   ), (20,3));
+      --array_counter : Integer := 1;
       sums : array (1 .. 20) of Orka.Float_32 := (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+      Losses_Sum : Orka.Float_32 := 0.0;
 
    begin
-      --find max value of each row and set it to pos in respective target array
+      --1a. find max value of each row and set it to pos in respective maximum array
+
       --for i in tensors rows
          for I in 1..(X.Shape(1)) loop
             --for j in tensors columns
@@ -26,77 +51,81 @@ package body Mlengine.LossFunctions is
 
             end loop;
 
-            --target is maximums of each tensor row
-            Target(array_counter) := (X((I,J)));
-            array_counter := array_counter + 1;
+            --maximums is maximums of each tensor row
+            Maximums(I) := (X((I,J)));
+            --move thru maximums array
+            --array_counter := array_counter + 1;
+            --reset max for next row
             maximum := 0.0;
             
          end loop;
 
       --reset array counter
-      array_counter := 1;
+      --array_counter := 1;
 
-      --calc exponentials
+      --1b. calc exponentials of (X - Max of its row) adn assign to Un_Prob
+
+      --for i in tensors rows
       for I in 1..(X.Shape(1)) loop
             --for j in tensors columns
             for J in 1..(X.Shape(2)) loop
-               --needs to be part of type
-               Proba.Set (((I,J)), (e ** (X(I,J) - Target(array_counter))));
+               
+               Unnormalized_Proba.Set (((I,J)), (e ** (X(I,J) - Maximums(I))));
 
             end loop;
             --increment to next max for row
-            array_counter := array_counter + 1;
+            --array_counter := array_counter + 1;
 
             
-         end loop;
+      end loop;
 
+      --reset array counter
+      --array_counter := 1;
 
-
-
-          --reset array counter
-      array_counter := 1;
-
-      --sum unormalized probs
-      for I in 1..(Proba.Shape(1)) loop
+      --2a. sum unormalized probs and store in sums array
+      --for i in tensors rows
+      for I in 1..(Unnormalized_Proba.Shape(1)) loop
             --for j in tensors columns
-            for J in 1..(Proba.Shape(2)) loop
+            for J in 1..(Unnormalized_Proba.Shape(2)) loop
                
               sums(I) := (sums(I)+X(I,J));
 
             end loop;
             
             
-         end loop;
+      end loop;
 
-      --divide unormal proba by sum
-      for I in 1..(Proba.Shape(1)) loop
+      --2b. divide Un_Proba by sum for each row and assign to Proba
+      --for i in tensors rows
+      for I in 1..(Unnormalized_Proba.Shape(1)) loop
             --for j in tensors columns
-            for J in 1..(Proba.Shape(2)) loop
+            for J in 1..(Unnormalized_Proba.Shape(2)) loop
                
-              Proba.Set (((I,J)), (Proba(I,J) / Sums(I)));
+              E.Proba.Set (((I,J)), (Unnormalized_Proba(I,J) / Sums(I)));
 
             end loop;
             
             
-         end loop;
-
-      
-
-      -- calculate the loss based on the predicted probabilities
-      Loss := 0.0;
-      for k in Targets'Range loop
-         Loss := Loss - Log (Probabilities (Targets (K)));
-      end loop;
-      Loss := Loss / Float (Targets'Length);  -- Average loss over all targets
-
-      Put_Line ("Loss: " & Float'Image (Loss));
-
-      -- display probabilities
-      for K in Probabilities'Range loop
-         Put_Line ("Probability: " & Float'Image (Probabilities (K)));
       end loop;
 
-      --Assert (Loss = Expected_Loss, "Loss is incorrect");
+      --3a. assign target to E.Target
+      E.Target := Target;
+
+      --4a. iterate thru target, taking target of each row inside E.Proba
+      --and takin ghe negative loss of each, appending it to losses array
+      for I in Target'Range loop
+         Losses(I) := (-log(E.Proba(I,Target(I))));
+      end loop;
+
+      --5a. calculate sum of losses
+      for I in Target'Range loop
+         Losses_Sum := Losses_Sum + Losses(I);
+      end loop;
+
+      --5b. return loss mean (sums / total num)
+      return (Losses_Sum / 20);
+
+   
 
    end Forward;
 
