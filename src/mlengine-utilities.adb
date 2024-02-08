f719d25c55a45cdd28b28512894f3c7fa4f8177e
samleo312 <samleo312@gmail.com>
2024-02-08 12:34:32 -0800

    setup fit function, needs vetted

--- src/mlengine-utilities.adb
+++ src/mlengine-utilities.adb
@@ -1,6 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Mlengine.Operators;
 with Mlengine.Optimizers;
+with Mlengine.Utilities; use Mlengine.Utilities;
 with Orka;
 use Orka;
 with AUnit.Reporter.Text;
@@ -12,6 +13,7 @@ package body Mlengine.Utilities is
     procedure Add(M : in out Model; Layer: Func_Access_T) is
     begin
         M.Graph.Append(Layer);
+        M.Parameters.Append(Layer.Get_Params);
     end;
 
     procedure InitializeNetwork(M : in out Model) is
@@ -21,27 +23,49 @@ package body Mlengine.Utilities is
         end loop; 
     end;
 
-    --function Fit(M: in out Model; Data : Tensor; Target : Tensor; Batch_Size : Integer; Num_Epochs : Integer; Optimizer : SGD ; Loss_Fn : SoftmaxWithLoss) is
-    --begin
-    --    InitializeNetwork(M);
-        --imported data from python func here somehow
-        --Data_Gen = 
-    --    for Epoch in 1 .. Num_Epochs loop
-    --        for I in 1 .. Data_Gen'Length loop
-    --            Optimizer.zero_grad;
-    --            for F in M.Graph.First_Index .. M.Graph.Last_Index loop
+    function Fit(M: in out Model; Data : Tensor; Target : Tensor; Batch_Size : Integer; Num_Epochs : Integer; Optimizer : SGD ; Loss_Fn : SoftLossMax_T) is
+        Gen_Data : DataGenerator;
+        LossHistory : Float_Vector.Vector;
+        Itr : Integer := 0;
+    begin
+        InitializeNetwork(M);
 
-     --               X := M.Graph(F).Forward(X);
-                    --stef needs to finish his forward and backward and call from a loss_func object like how relu and linear r set up
-    --            end loop;
-    --            Optimizer.step;
-    --        end loop;
-    --    end loop;
-    --end;
 
-    --function Predict(M : in out Model; Data : Tensor) is
-    --    X : Tensor := Data;
-    --begin
+        Gen_Data.Data.Data := ST.CPU.To_Tensor((0.05419138, 0.10842358, -0.16752893, 0.561395, 0.7311231, 0.60580504, -0.26001516, -0.46796957, 0.5362811, -0.39603913, 0.26051527, 0.15478744, -0.10809994, 0.5860736, 0.05174225, -0.04818951, -0.24336624, 0.09871892, 0.14073071, -0.86744624, 0.35767263, -0.78053826, -0.26843658, 0.35422122, 0.57703453, -0.35361794, -0.3348171, 0.11351098, -0.60558337, -0.32412556, -0.79294413, -0.08950639, -0.05379663, -0.359635, 0.5690315, -0.08774939, 0.00433535, 0.0604508, -0.8164081, -0.05384514), 
+                                            (20,2));
+        Gen_Data.Target := (1, 3, 3, 2, 1, 1, 3, 2, 3, 1, 1, 3, 1, 3, 2, 2, 2, 1, 1, 2);
+
+        for Epoch in 1 .. Num_Epochs loop
+            for I in 1 .. Data_Gen.Target'Range loop
+                Optimizer.zero_grad;
+                for F in M.Graph.First_Index .. M.Graph.Last_Index loop
+                    Data_Gen.Data (I) := F.Forward;
+                end loop;
+                
+                declare
+                    Loss : Orka.Float_32 := Forward(Loss_Fn, Data_Gen.Data, Data.Gen.Target);
+                    Grad : Tensor;
+                begin
+                    Grad.Data := new ST_CPU.CPU_Tensor'(Loss_Fn.Backward);
+
+                    for F in reverse M.Graph'Range loop
+                        Grad.Data.all := F.Backward(Grad);
+                    end loop;
+
+                    LossHistory.Append(Loss);
+                    Itr := Itr + 1; 
+                end;
+
+                
+                Optimizer.step;
+            end loop;
+        end loop;
+        return LossHistory;
+    end;
+
+    function Predict(M : in out Model; Data : Tensor) is
+        X : Tensor := Data;
+    begin
         
-    --end;
+    end;
 end Mlengine.Utilities;
--- src/mlengine-utilities.ads
+++ src/mlengine-utilities.ads
@@ -4,6 +4,7 @@ with Mlengine.Optimizers; use Mlengine.Optimizers;
 with Ada.Containers;
 with Ada.Containers.Vectors;
 
+
 package Mlengine.Utilities is
 
    package Layer_Vector is new
@@ -13,14 +14,44 @@ package Mlengine.Utilities is
 
    use Layer_Vector;
 
+   package Float_Vector is new
+     Ada.Containers.Vectors
+       (Index_Type   => Natural,
+        Element_Type => Orka.Float_32);
+
+   use Float_Vector;
+
+   package Param_Vector is new
+     Ada.Containers.Vectors
+       (Index_Type   => Natural,
+        Element_Type => ParamsArray);
+
+   use Param_Vector;
+
+
+   -- Array size needs to be equal to Batch Size, how to dynamically change type definition.
+   -- If variable is in type definition, does editing the variable change the size capability of the array.
+   type Target_Array is array (1 .. 20) of Standard.Integer;
+   
+
+
    type Model is record
       Graph : Layer_Vector.Vector;
+      Parameters : Param_Vector.Vector;
+   end record;
+
+   type DataGenerator is record
+      Data : Tensor;
+      Target : Target_Array;
+      Batch_Size : Integer;
+      Num_Batches : Integer;
+      Counter : Integer;
    end record;
 
    procedure Add(M : in out Model; Layer: Func_Access_T);
    procedure InitializeNetwork(M : in out Model);
-   --function Fit(M: in out Model; Data : Tensor; Target : Tensor; Batch_Size : Integer; Num_Epochs : Integer; Optimizer : SGD ; Loss_Fn : SoftmaxWithLoss);
-   --function Predict(M : in out Model; Data : Tensor);
+   function Fit(M: in out Model; Data : Tensor; Target : Tensor; Batch_Size : Integer; Num_Epochs : Integer; Optimizer : SGD ; Loss_Fn : SoftLossMax_T);
+   function Predict(M : in out Model; Data : Tensor);
    
 
 end Mlengine.Utilities;
